1ee2a8e07241ed0c911bd4fc3e58690cceabda68
Moon <118666310+milkycocomoon@users.noreply.github.com>
2023-02-13 23:15:44 +0300

    feat: compeleted task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = [];
+
+  for(let i=0; i<bracketsConfig.length; i++){
+    brackets[i] = bracketsConfig[i].join('');
+  }
+
+  let before = '';
+  while(str.length != before.length){
+    before = str;
+    for(let i=0; i<brackets.length; i++){
+      str = str.replace(brackets[i], '');
+    }
+  }
+  return (str.length > 0) ? false : true;
 }
